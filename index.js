f84e295b77631f76087c499d4aac5d830ee1cedb
Future <futureworldzone@gmail.com>
2023-03-30 16:00:11 +0300

    Completed assignment with required changes from the README.md file on the lab for JavaScript Arrow Functions

---
+++ index.js
@@ -0,0 +1,18 @@
+// Code your solution in this file!
+
+//using the information from README.md and testindex.js
+
+// has a function expression called divide
+// divide divides 2000 by 100
+let divide = () => 2000/100
+//for an empty arrow function, write brackets before the >= signs like this: ()
+
+//has an arrow function called square
+//square arrow function takes one parameter and multiplies it times itself
+let square = x => x * x;
+//for a function to pick only one parameter, write the name before the >= signs
+
+//has an arrow function called add 
+//add arrow function takes two parameters and adds them together
+let add = (a, b) => a + b;
+//for a function with 2 or more parameters, write them in the brackets
